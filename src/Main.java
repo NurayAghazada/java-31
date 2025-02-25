4e483e83361f7761d64cb8648d2f056c8ae98656
nuray-aghazada <nurayaghazade1717@outlook.com>
2025-02-25 16:29:27 +0400

    this is my initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I'm in Master Branch.gi");
+        System.out.println("I'm in Master Branch.");
     }
 }
